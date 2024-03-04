095543d93b64725ebf28d8ede724fc2085d88dd8
Jose Campo Rivas <josecampor31@gmail.com>
2024-03-04 22:52:17 +0100

    add CORS to backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routes import users_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # TODO: Change this to the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users_router.router)
 
 
