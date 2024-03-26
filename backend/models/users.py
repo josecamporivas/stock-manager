5e2c73ef5c50ec69b8d2d9cabc6901a119ed1e0a
Jose Campo Rivas <josecampor31@gmail.com>
2024-03-26 23:55:18 +0100

    Update user password reparated

--- backend/models/users.py
+++ backend/models/users.py
@@ -2,6 +2,7 @@ from schemas.User import User, UserCreate
 from utils.auth import verify_password, get_password_hash
 from utils.db import connect
 
+
 class Users:
     @staticmethod
     async def all():
@@ -19,16 +20,18 @@ class Users:
                 result = cursor.fetchone()
                 return result
 
-#    @staticmethod
-#    async def get_actual_user(current_user: Annotated[User, Depends(get_current_user)]):
-#        return current_user
+    #    @staticmethod
+    #    async def get_actual_user(current_user: Annotated[User, Depends(get_current_user)]):
+    #        return current_user
 
     @staticmethod
     async def create(data: UserCreate):
         with connect() as connection:
             with connection.cursor() as cursor:
-                cursor.execute('INSERT INTO users (dni, username, password, name, surname, email, role) VALUES (%s, %s, %s, %s, %s, %s, %s)',
-                               (data.dni, data.username, get_password_hash(data.password), data.name, data.surname, data.email, data.role))
+                cursor.execute(
+                    'INSERT INTO users (dni, username, password, name, surname, email, role) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                    (data.dni, data.username, get_password_hash(data.password), data.name, data.surname, data.email,
+                     data.role))
                 connection.commit()
                 user_id = cursor.lastrowid
                 return await Users.get(user_id)
@@ -37,8 +40,15 @@ class Users:
     async def update(id_user: int, data: UserCreate):
         with connect() as connection:
             with connection.cursor() as cursor:
-                cursor.execute('UPDATE users SET dni=%s, username=%s, password=%s, name=%s, surname=%s, email=%s, role=%s WHERE user_id=%s',
-                               (data.dni, data.username, get_password_hash(data.password), data.name, data.surname, data.email, data.role, id_user))
+                if data.password:
+                    cursor.execute(
+                        'UPDATE users SET dni=%s, username=%s, password=%s, name=%s, surname=%s, email=%s, role=%s WHERE user_id=%s',
+                        (data.dni, data.username, get_password_hash(data.password), data.name, data.surname, data.email,
+                         data.role, id_user))
+                else:
+                    cursor.execute(
+                        'UPDATE users SET dni=%s, username=%s, name=%s, surname=%s, email=%s, role=%s WHERE user_id=%s',
+                        (data.dni, data.username, data.name, data.surname, data.email, data.role, id_user))
                 connection.commit()
                 return await Users.get(id_user)
 
